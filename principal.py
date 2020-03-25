cd6d8a3ef7b97162fc70515b8e0a4b88248da60e
Mauricio Anaya <46908589+mauanaya@users.noreply.github.com>
2020-03-25 12:24:47 -0600

    Add files via upload

--- principal.py
+++ principal.py
@@ -7,8 +7,11 @@
 
 import funciones as fn
 #%%
-
 datos = fn.f_leer_archivo(param_archivo='archivo_tradeview_1.xlsx')
 fn.f_pip_size(param_ins='audusd')
 datos = fn.f_columnas_tiempo(param_data=datos)
 datos = fn.f_columnas_pips(param_data=datos)
+datos = fn.f_columnas_capital_acum(param_data=datos)
+df_1 = fn.f_estadisticas_ba(param_data=datos)
+
+
